45c8c713f03adafe97af026374ad4e22ad8e5932
Emilie Hopkinson <emilie.n.hopkinson.22@dartmouth.edu>
2024-04-30 23:26:31 -0400

    basic ui layout

--- daily-dose/src/App.jsx
+++ daily-dose/src/App.jsx
@@ -1,13 +1,84 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
+
+      <div className="navBar"> 
+        <div className="logoText">
+          Daily Dose
+        </div>
+      </div>
+
+      <div className="titleLines">
+        <div className="calculatorTypeLogo">
+          <img className="heart" src='heartPulse.png' alt="heart"/>
+        </div>
+        <div className="titleTexts">
+          <div className="mainTitle">
+            Daily Caloric Intake Calculator
+          </div>
+          <div className="subTitle">
+            Calculator how much of one food meets your daily needs
+          </div>
+
+        </div>
+
+        
+      </div>
+      <div className="mainBody">
+
+        <div className="leftBody">
+          <div className="selectOpt">
+            <div className="selectTitle">
+              Brand:
+            </div>
+            <div className="selectInput">
+              Generic
+            </div>
+          </div>
+          <div className="selectOpt">
+            <div className="selectTitle">
+              Food Type:
+            </div>
+            <div className="selectInput">
+              Carrots
+            </div>
+          </div>
+          <div className="selectOpt">
+            <div className="selectTitle">
+              Quantity (optional):
+            </div>
+            <div className="quantity">
+              12
+            </div>
+          </div>
+          <div className="buttonWrapper">
+            <div className="submitButton">
+              Calculate
+            </div>
+
+          </div>
+
+        </div>
+        
+
+        <div className="rightBody">
+          right
+        </div>
+      
+      </div>
+      <div className="footer"> footer</div>
+
+
+
+
+{/*     
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -27,7 +98,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
